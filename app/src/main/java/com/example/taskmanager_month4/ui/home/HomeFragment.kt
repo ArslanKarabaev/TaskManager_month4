101a945b8d2e0b1394f8cf505646668a511c499e
ArslanKarabaev <arslankarabaev08@gmail.com>
2023-09-19 14:17:53 +0600

    zebra and isEmpty

--- app/src/main/java/com/example/taskmanager_month4/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/taskmanager_month4/ui/home/HomeFragment.kt
@@ -6,12 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.taskmanager_month4.R
 import com.example.taskmanager_month4.databinding.FragmentHomeBinding
+import com.example.taskmanager_month4.model.Task
+import com.example.taskmanager_month4.ui.task.TaskFragment.Companion.RESULT_KEY
+import com.example.taskmanager_month4.ui.task.TaskFragment.Companion.TASK_KEY
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private val adapter = TaskAdapter()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,6 +33,19 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.adapter = adapter
+        setFragmentResultListener(RESULT_KEY){
+            requestKey, bundle ->
+            val data = bundle.getSerializable(TASK_KEY) as Task
+            adapter.setData(data)
+        }
+        binding.fab.setOnClickListener{
+            findNavController().navigate(R.id.taskFragment)
+        }
     }
 
     override fun onDestroyView() {
