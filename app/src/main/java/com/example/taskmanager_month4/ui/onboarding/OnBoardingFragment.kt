5b22f904c82542b3baa15ac84f4742acd2ccf832
ArslanKarabaev <arslankarabaev08@gmail.com>
2023-09-22 12:31:18 +0600

    lesson 3,HW

--- app/src/main/java/com/example/taskmanager_month4/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/example/taskmanager_month4/ui/onboarding/OnBoardingFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.taskmanager_month4.R
 import com.example.taskmanager_month4.databinding.FragmentOnboardingBinding
 import com.example.taskmanager_month4.ui.onboarding.adapter.OnBoardingAdapter
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 
 class OnBoardingFragment : Fragment() {
     private lateinit var binding: FragmentOnboardingBinding
@@ -22,6 +24,9 @@ class OnBoardingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewPager.adapter = adapter
+        val indicator = view.findViewById<WormDotsIndicator>(R.id.indicator)
+        indicator.attachTo(binding.viewPager)
+
     }
 
     private fun onClick(){
--- app/src/main/java/com/example/taskmanager_month4/ui/onboarding/adapter/OnBoardingAdapter.kt
+++ app/src/main/java/com/example/taskmanager_month4/ui/onboarding/adapter/OnBoardingAdapter.kt
@@ -7,13 +7,17 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.taskmanager_month4.databinding.ItemOnboardingBinding
 import com.example.taskmanager_month4.model.OnBoarding
+import com.bumptech.glide.Glide as Glide
 
 class OnBoardingAdapter(private val onClick:() -> Unit) : Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
 
     val data = arrayListOf(
-        OnBoarding("", "Title 1", "Desc 1"),
-        OnBoarding("", "Title 2", "Desc 2"),
-        OnBoarding("", "Title 3", "Desc 3"),
+        OnBoarding("https://thumbs.dreamstime.com/b/%D0%BA%D0%BE%D0%BD%D1%86%D0%B5%D0%BF%D1%86%D0%B8%D1%8F-%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D1%82%D0%BE%D1%80%D0%B3%D0%BE%D0%B2%D0%BB%D0%B8-%D0%B2-%D1%81%D0%BE%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D0%BC-%D0%BF%D0%BB%D0%BE%D1%81%D0%BA%D0%BE%D0%BC-%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD%D0%B5-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D0%B0-230895711.jpg",
+            "Покупай не выходя из дома", "Выбирай любой магазин и совершай покупки онлайн "),
+        OnBoarding("https://www.obyavleniya-moskva.ru/components/com_djclassifieds/images/profile/1/1070_e4rqbp2npq52i38w2zj3tqf8pxkp3-rncr0z50n1kyfwosdhimtexsxcggzbl_u7egtaln-jom6cyqiwru5fskwz.jpg",
+            "Доставка", "Укажи адресс доставки и мы привезем тебе твои новые покупки"),
+        OnBoarding("https://proftest.me/wp-content/uploads/2020/07/what-is-your-happiness.jpg",
+            "Начало пути", "Так давай же начнем исследовать..."),
     )
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
        return OnBoardingViewHolder(ItemOnboardingBinding.inflate(LayoutInflater.from(parent.context),parent, false ))
@@ -34,10 +38,15 @@ class OnBoardingAdapter(private val onClick:() -> Unit) : Adapter<OnBoardingAdap
          binding.btnStart.setOnClickListener{
              onClick()
          }
+             binding.btnSkip.setOnClickListener{
+                 onClick()
+             }
              binding.btnStart.isVisible = adapterPosition == data.lastIndex
+             binding.btnSkip.isVisible = adapterPosition != data.lastIndex
              binding.tvTitle.text = onBoarding.title
              binding.tvDesc.text = onBoarding.desc
-             // TODO: bind image
+             Glide.with(binding.imgOnboard).load(onBoarding.image).into(binding.imgOnboard)
+
          }
 
     }
